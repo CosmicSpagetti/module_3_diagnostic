cf3df20f4ea11c5304b08556459ef2ca0944fdef
billy urrutia <billyurrutia@gmail.com>
2019-07-08 11:00:29 -0600

    got a working connection

--- app/facade/foods_facade.rb
+++ app/facade/foods_facade.rb
@@ -4,16 +4,26 @@ class FoodsFacade
   end
 
   def food_count 
-    conn = Faraday.new(:url => 'https://api.nal.usda.gov/ndb/search/') do |faraday|
+    response = food_service.get 
+    body = JSON.parse(response.body, symbolize_names: true)
+    body[:list][:item].count
+  end
+
+  def foods
+    response = food_service.get 
+    body = JSON.parse(response.body, symbolize_names: true)
+    body[:list][:item].each do |food_item|
+      Food.new(food_item) 
+    end
+  end
+
+  private 
+
+  def food_service
+     Faraday.new(:url => "https://api.nal.usda.gov/ndb/search/?q=#{@food}") do |faraday|
       faraday.params['format'] = 'json'
       faraday.params['api_key'] = ENV['FOOD_API_KEY'] 
       faraday.adapter Faraday.default_adapter
     end
-    binding.pry
-    conn.get "#{@food}"
-  end
-
-  def foods
-    
   end
 end
